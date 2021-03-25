7f1bfd37fcd54afea11b0dbeb1a18c69fd6c63a2
Spencer Henry <spencerhenry94@gmail.com>
2021-03-25 16:04:48 -0400

    created ability to add a task to each time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@ var day = moment().format("MM/DD/YYYY"); // Display current day
 //console.log(day);
 
 $('#currentDay').html(day); // Add current day to html
+
+// Add text input to hour blocks.
+$(".hour-description").on("click", "p", function() {
+    var text = $(this)
+    .text()
+    .trim();
+
+    var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
