bb3cec8a15792aec8e6d40fe2b2415b2e42a887b
Spencer Henry <spencerhenry94@gmail.com>
2021-03-25 21:02:24 -0400

    added script to change block color based on time of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,20 @@ $(".hour-description").on("click", "p", function() {
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
 });
+
+function colorChange() {
+    var d = new Date();
+    var n = d.getHours()+1;
+    console.log(n);
+
+    if (n > 17 || n < 9) {
+        // If time is after 5PM or before 9AM, apply night theme to hour text
+        $(".hour-block").addClass("night").removeClass("daytime");
+    } else {
+        // Apply the day theme
+        $(".hour-block").addClass("daytime").removeClass("night");
+    }
+}
+
+colorChange();
+
