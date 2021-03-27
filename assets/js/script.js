1048b56d933f846be97db9268efc947ffaaa922b
Spencer Henry <spencerhenry94@gmail.com>
2021-03-27 12:20:05 -0400

    added sample code for saving to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var day = moment().format("MM/DD/YYYY"); // Display current day
 $('#currentDay').html(day); // Add current day to html
 
 // Add text input to hour blocks.
-$(".hour-description").on("click", "p", function() {
+$(".hour-description").on("click", "p", function () {
     var text = $(this)
     .text()
     .trim();
@@ -15,12 +15,19 @@ $(".hour-description").on("click", "p", function() {
 
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
+
 });
 
+//store to local storage
+    /*var myTask = JSON.stringify($(".hour-description"));
+    console.log(myTask);*/
+
+
+// Change hourly block colour based on current time of day
 function colorChange() {
     var d = new Date();
     var n = d.getHours()+1;
-    console.log(n);
+    //console.log(n);
 
     if (n > 17 || n < 9) {
         // If time is after 5PM or before 9AM, apply night theme to hour text
@@ -33,3 +40,10 @@ function colorChange() {
 
 colorChange();
 
+// save text to local storage when save button is clicked
+/*$(".save").on("click", "button",function() {
+    var str = JSON.stringify($(".hour-description"));
+    localStorage.setItem("text", str);
+    console.log(function
+
+});*/
